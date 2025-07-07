f1d80c259d0cabff1a2467d6543a112d18224641
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-07-07 22:34:16 +0530

    test

--- src/CodeContext/CodeContextInput.jsx
+++ src/CodeContext/CodeContextInput.jsx
@@ -1,9 +1,7 @@
 
 
 
-//   return (
-//     <div className="bg-[#1F2937] rounded-lg px-4 pt-4 pb-2 border-2 border-gray-700 w-full max-w-6xl mx-auto">
-//       <div className="flex items-center space-x-2 mb-2">
+
 //         <Code2 className="w-5 h-5 text-white" />
 //         <h3 className="text-2xl font-semibold">
 //           Code Context <span className="text-gray-400 text-base">(Optional)</span>
