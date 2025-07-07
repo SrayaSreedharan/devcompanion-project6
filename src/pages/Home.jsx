10b74d532553a95a115c6edef2093e337135527d
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-07-07 22:00:36 +0530

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../layout/Header';
+import DevCompanion from '../components/DevCompanion';
 
 
 const Home = () => {
@@ -7,7 +8,9 @@ const Home = () => {
     <div className="bg-[#0F172A] min-h-screen text-white px-4 py-6">
       <div className="max-w-6xl mx-auto space-y-6">
         <Header />
-       
+        <main className="mt-4">
+        <DevCompanion />
+      </main>
       </div>
     </div>
   );
