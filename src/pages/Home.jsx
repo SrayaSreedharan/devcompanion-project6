3ce21f300c2e7536cbd4ef7857a3383c0dbf7f43
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-07-11 16:17:37 +0530

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,10 @@ import Header from '../layout/Header';
 import DevCompanion from '../components/DevCompanion';
 import Warningmessage from '../components/Warningmessage';
 
-
-
 const Home = () => {
   return (
    <div className="bg-[#0F172A] min-h-screen text-white px-4 py-6">
-      <Warningmessage/> {/* 🔁 Show popup here once on app load */}
+      <Warningmessage/> 
       <div className="max-w-6xl mx-auto space-y-6">
         <Header />
         <main className="mt-4">
