bfe4b45010e8993df983fbc3eea62fa804f0629c
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-07-11 10:11:59 +0530

    test

--- public/background.js
+++ public/background.js
@@ -1,7 +1,15 @@
 /* global chrome */
 
-if (typeof chrome !== "undefined" && chrome.runtime) {
-  chrome.runtime.sendMessage({ action: "getData" }, (response) => {
+chrome.runtime.sendMessage({ action: "getData" }, (response) => {
+  if (chrome.runtime.lastError) {
+    console.error("Message error:", chrome.runtime.lastError.message);
+    return;
+  }
+
+  if (response && response.result) {
     console.log(response.result);
-  });
-}
+  } else {
+    console.warn("No result received");
+  }
+});
+
